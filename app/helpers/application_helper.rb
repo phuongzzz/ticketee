f09734fb571a11949140010d1b0529c49bd7e9ca
phuongzzz <yesterdayoncemore23@gmail.com>
2017-11-24 21:23:24 +0700

    add dynamic title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def page_title(*parts)
+    unless parts.empty?
+      content_for :custom_page_title do
+        (parts << "Ticketee").join(" - ")
+      end
+    end
+  end
 end
